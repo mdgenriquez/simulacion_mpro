4c159c4e968d4c7ad0d08ebed66e8f1de979173c
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-03-27 16:13:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -83,6 +83,10 @@ selected_features_mask = selector_LGBM.support_
 Selected_features = RDKit_PaDEL_df_columns[selected_features_mask]
 RDKit_PaDEL = RDKit_PaDEL_scaled_df[Selected_features]
 
+# Make predictions
+predictions = lgbm_model.predict(RDKit_PaDEL)
+st.write("Predicción de Docking score")
+st.dataframe(predictions)
 
 
 
