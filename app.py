80f1224f552e6961fb4322206cb810596555d65d
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-03-27 15:58:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,3 +34,30 @@ selector_Huber = joblib.load('archivos/selector_Huber.pickle')
 selector_LGBM = joblib.load('archivos/selector_LGBM.pickle')
 svr_best_model = joblib.load('archivos/svr_best_model.pickle')
 
+# RDKit selected descriptors function
+def get_selected_RDKitdescriptors(smile, selected_descriptors, missingVal=None):
+    ''' Calculates only the selected descriptors for a molecule '''
+    res = {}
+    mol = Chem.MolFromSmiles(smile)
+    if mol is None:
+        return {desc: missingVal for desc in selected_descriptors}
+
+    for nm, fn in Descriptors._descList:
+        if nm in selected_descriptors:
+            try:
+                res[nm] = fn(mol)
+            except:
+                import traceback
+                traceback.print_exc()
+                res[nm] = missingVal
+    return res
+
+df = pd.DataFrame({'smiles': [compound_smiles]})
+#st.dataframe(df)
+
+# Calculate selected RDKit descriptors
+RDKit_descriptors = [get_selected_RDKitdescriptors(m, RDKit_select_descriptors) for m in df['smiles']]
+RDKit_df = pd.DataFrame(RDKit_descriptors)
+st.write("Descriptores RDKit")
+st.dataframe(RDKit_df)
+
