2632512fabf39d442e563583083bdc23e3ab0bb8
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-03-27 16:42:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,13 +84,12 @@ Selected_features = RDKit_PaDEL_df_columns[selected_features_mask]
 RDKit_PaDEL = RDKit_PaDEL_scaled_df[Selected_features]
 
 # Selected features--prueba
-selected_features_mask = select_LGBM.support_
+selected_features_mask = select_hgb.support_
 Selected_features = RDKit_PaDEL_df_columns[selected_features_mask]
 RDKit_PaDEL = RDKit_PaDEL_scaled_df[Selected_features]
 
-
 # Make predictions ERROR
-predictions = LGBM_model.predict(RDKit_PaDEL)
+predictions = hgb_model.predict(RDKit_PaDEL)
 st.write("Predicción de Docking score")
 st.dataframe(predictions)
 
